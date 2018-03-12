1f288349ed843c553749eb43da627de612094c88
hunkxiaoye <yang3284627@vip.qq.com>
2018-03-12 23:13:42 +0800

    读写分离（改）

--- src/main/java/com.userDemo/common/database/DataSourceAspect.java
+++ src/main/java/com.userDemo/common/database/DataSourceAspect.java
@@ -1,11 +1,16 @@
 package com.userDemo.common.database;
 
 import java.lang.reflect.Method;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
+
 public class DataSourceAspect {
     public void pointCut() {
-    };
+    }
+
+    ;
+
     public void before(JoinPoint point) {
         Object target = point.getTarget();// 拦截的实体类
         String method = point.getSignature().getName();// 拦截的方法名称
@@ -16,6 +21,21 @@ public class DataSourceAspect {
             if (m != null && m.isAnnotationPresent(DataSource.class)) {
                 DataSource data = m.getAnnotation(DataSource.class);
                 HandleDataSource.putDataSource(data.value());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void after(JoinPoint point) {
+        Object target = point.getTarget();// 拦截的实体类
+        String method = point.getSignature().getName();// 拦截的方法名称
+        Class[] classz = target.getClass().getInterfaces();
+        Class[] parameterTypes = ((MethodSignature) point.getSignature()).getMethod().getParameterTypes();// 拦截的方法参数类型
+        try {
+            Method m = classz[0].getMethod(method, parameterTypes);
+            if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                HandleDataSource.remove();
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com.userDemo/common/database/HandleDataSource.java
+++ src/main/java/com.userDemo/common/database/HandleDataSource.java
@@ -1,11 +1,18 @@
 package com.userDemo.common.database;
 
 public class HandleDataSource {
+
     public static final ThreadLocal holder = new ThreadLocal();
+
     public static void putDataSource(String datasource) {
         holder.set(datasource);
     }
-        public static Object getDataSource() {
+
+    public static Object getDataSource() {
         return holder.get();
+    }
+
+    public static void remove() {
+        holder.remove();
     }
 }
